4bbd969046b71d04651e72c9b3405d38033bac7b
mahlet-16 <mahletamenu16@gmail.com>
2025-04-02 08:57:36 +0300

    Update serializers.py

--- event_management/events/serializers.py
+++ event_management/events/serializers.py
@@ -1,6 +1,24 @@
 from rest_framework import serializers
 from django.utils.timezone import now
 from .models import Event, Category, Registration, Location
+from django.contrib.auth.models import User
+
+
+# User Registration Serializer
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
