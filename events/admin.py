16cae5c91ed24c8abd75c54a151b59507fb3581d
mahlet-16 <mahletamenu16@gmail.com>
2025-04-05 23:06:17 +0300

    finishing the project

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import Registration, Event, Location, Category
 
 # Register your models here.
-
-admin.site.register(Registration)
-admin.site.register(Event)
-admin.site.register(Location)
-admin.site.register(Category)
