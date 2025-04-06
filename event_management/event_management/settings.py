572026d937366b2d316436dff3b2ef33b89f26cf
mahlet-16 <mahletamenu16@gmail.com>
2025-04-06 16:39:43 +0300

    Update settings.py

--- event_management/event_management/settings.py
+++ event_management/event_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-55a&_6n^tfh0*6k$&lxnm^w49e$8hu2-dpvp^_l0is6*npzti#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mahlet16.pythonanywhere.com']
 
 
 # Application definition
